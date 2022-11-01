157797faa3a7eeb956a89a30a2b283a72d83b928
Celia Bocking <celiabocking99@gmail.com>
2022-11-01 12:09:57 +0000

    ticket 2 done

--- scrabble-score.js
+++ scrabble-score.js
@@ -1,11 +1,23 @@
 export function calculateScrabbleScore(word) {
     let scoreArray = [['A',1],['B',3],['C',3],['D',2],['E',1],['F',4],['G',2],['H',4],['I',1],['J',8],['K',5],['L',1],['M',3],['N',1],['O',1],['P',3],['Q',10],['R',1],['S',1],['T',1],['U',1],['V',4],['W',4],['X',8],['Y',4],['Z',10]]
-   
+    let score = 0 
 
-    for(let i=0; i<scoreArray.length;i++){
-        if(scoreArray[i][0]===word){
-            return scoreArray[i][1]
-        }
+    for(let i=0; i<word.length; i++){
+        for(let j = 0; j<scoreArray.length; j++){
+            if(word[i].toUpperCase() === scoreArray[j][0]){
+                score += scoreArray[j][1]
+            }
+        }  
     }
+    return score
+} 
 
-}
+
+
+
+
+
+//write a for loop that iterates through characters in a string
+//find the corresponding key value pair for that character and add teh value to a variable (set to zero)
+
+
--- scrabble-score.test.js
+++ scrabble-score.test.js
@@ -7,3 +7,10 @@ import { calculateScrabbleScore } from "./scrabble-score";
 test.each([['A',1],['B',3],['C',3],['D',2],['E',1],['F',4],['G',2],['H',4],['I',1],['J',8],['K',5],['L',1],['M',3],['N',1],['O',1],['P',3],['Q',10],['R',1],['S',1],['T',1],['U',1],['V',4],['W',4],['X',8],['Y',4],['Z',10]])('.calculateScrabbleScore(%s,%i)',(a,b)=>{
     expect(calculateScrabbleScore(a)).toBe(b)
 })
+
+test('test if celia returns score of 7', ()=>{
+    const actual = calculateScrabbleScore('celia')
+    const expected = 7
+    expect(actual).toBe(expected)
+})
+
